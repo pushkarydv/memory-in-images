85b1fee8105f4b7995c5022fecc0f5563d9af6f4
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2025-03-03 06:20:04 +0530

    seo metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,8 +14,67 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: 'Image Mem',
-  description: 'Search for any moment in your images',
+  title: {
+    default: 'Memory in Images',
+  },
+  description: 'Search within your images using natural language.',
+  keywords: [
+    'Memory',
+    'Images',
+    'Search',
+    'Image Vector Search',
+    'Image Search',
+  ],
+
+  metadataBase: new URL('https://memory-in-images.vercel.app'),
+
+  openGraph: {
+    title: 'Memory in Images',
+    description: 'Search within your images using natural language.',
+    url: 'https://memory-in-images.vercel.app',
+    siteName: 'Memory in Images',
+    images: [
+      {
+        url: '/banner.jpg',
+        width: 960,
+        height: 540,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Memory in Images',
+    description: 'Search within your images using natural language.',
+    images: ['/banner.jpg'],
+    creator: '@PushkarYadavIn',
+    site: '@PushkarYadavIn',
+  },
+
+  icons: {
+    icon: [{ url: '/logo.png', sizes: '256x256', type: 'image/png' }],
+    apple: [{ url: '/logo.png', sizes: '256x256', type: 'image/png' }],
+    other: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/logo.png',
+      },
+    ],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
